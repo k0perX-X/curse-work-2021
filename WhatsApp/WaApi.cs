df7f42301e1d7bbd4e79b618331142df69ac2f5b
Никита Копылов <nekopylov@edu.hse.ru>
2021-05-28 11:43:21 +0500

    123

---
+++ WhatsApp/WaApi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+
+namespace WhatsApp
+{
+    public class WaApi
+    {
+        private string APIUrl = "";
+        private string token = "";
+
+        public WaApi(string aPIUrl, string token)
+        {
+            APIUrl = aPIUrl;
+            this.token = token;
+        }
+        //асинхронный метод для отправки POST запросов
+        public async Task<string> SendRequest(string method, string data)
+        {
+            string url = $"{APIUrl}{method}?token={token}";
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                var content = new StringContent(data, Encoding.UTF8, "application/json");
+                var result = await client.PostAsync("", content);
+                return await result.Content.ReadAsStringAsync();
+            }
+        }
+        //Отправка сообщений, отправляем json строку, которая фотримурется через библиотеку Newtonsoft.Json
+        public async Task<string> SendMessage(string chatId, string text)
+        {
+            var data = new Dictionary<string, string>()
+        {
+            {"chatId",chatId },
+            { "body", text }
+        };
+            return await SendRequest("sendMessage", JsonConvert.SerializeObject(data));
+        }
+    }
+}
