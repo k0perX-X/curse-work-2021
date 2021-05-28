016d67d161c8031b280d46e300a5daa0c519cc66
Николай Перминов <kolya-perminov@ya.ru>
2021-05-28 15:47:07 +0500

    Добавил города состоящие из 2 слов

--- curse-work-2021/Program.cs
+++ curse-work-2021/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Xml.Serialization;
 
 namespace Database
 {
@@ -8,8 +9,18 @@ namespace Database
         private static void Main(string[] args)
         {
             Processing.ReadCsv();
-            Console.WriteLine(Processing.DatabaseFindToString());
-            Console.WriteLine(Processing.DatabaseCitiesToString());
+            string s = Console.ReadLine();
+            while (s.Split()[0] != "exit")
+            {
+                Processing.Get(s, "Test", out bool onLastLetter, out bool cityIsUsed, out string outCity, out int letterNumberFromEnd,
+                    out string wikiUrl, out string yandexUrl, out string googleUrl, out string mapUrl,
+                    out (double latitude, double longitude) coordinateCity, out string photoUrl);
+                Console.WriteLine(
+                    $"onLastLetter {onLastLetter}, cityIsUsed {cityIsUsed}, outCity {outCity}, letterNumberFromEnd {letterNumberFromEnd}, wikiUrl {wikiUrl}, " +
+                    $"yandexUrl {yandexUrl}, googleUrl {googleUrl}, mapUrl {mapUrl}, coordinateCity.latitude {coordinateCity.latitude}, " +
+                    $"coordinateCity.longitude {coordinateCity.longitude}, photoUrl {photoUrl}");
+                s = Console.ReadLine();
+            }
             Console.ReadKey();
         }
     }
