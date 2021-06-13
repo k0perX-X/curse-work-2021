7196e33c7251224ae1265ddb7d067996712837ea
Orlov Yasha <orlovaska599@gmail.com>
2021-06-13 12:54:53 +0300

    Прод тел бот 2

---
+++ Logging.cs
@@ -0,0 +1,72 @@
+﻿namespace Database
+{
+    public class Logging
+    {
+        public static class Level
+        {
+            public static readonly LevelLogging DEBUG = new(0);
+            public static readonly LevelLogging INFO = new(1);
+            public static readonly LevelLogging ERROR = new(2);
+            public static readonly LevelLogging WARNING = new(3);
+            public static readonly LevelLogging FATAL = new(4);
+        }
+
+        public class LevelLogging
+        {
+            public LevelLogging(byte numberOfLevel) => _numberOfLevel = numberOfLevel;
+
+            private readonly byte _numberOfLevel;
+
+            public static implicit operator byte(LevelLogging d) => d._numberOfLevel;
+        }
+
+        public Logging(LevelLogging levelLogging, string pathToLogFile)
+        {
+            if (levelLogging < 0 || levelLogging > 4)
+                levelLogging = new(1);
+            _levelLogging = levelLogging;
+            FileInfo fileInf = new FileInfo(pathToLogFile);
+            if (!fileInf.Exists)
+                fileInf.Create();
+            _pathToLogFile = pathToLogFile;
+        }
+
+        private string _pathToLogFile;
+        private int _levelLogging;
+
+        public void DEBUG(string message = "")
+        {
+            if (_levelLogging >= Level.DEBUG)
+                File.AppendAllText(_pathToLogFile,
+                    $"{DateTime.Now.ToShortTimeString()} DEBUG: {message}");
+        }
+
+        public void INFO(string message = "")
+        {
+            if (_levelLogging >= Level.INFO)
+                File.AppendAllText(_pathToLogFile,
+                    $"{DateTime.Now.ToShortTimeString()} INFO: {message}");
+        }
+
+        public void ERROR(string message = "")
+        {
+            if (_levelLogging >= Level.ERROR)
+                File.AppendAllText(_pathToLogFile,
+                    $"{DateTime.Now.ToShortTimeString()} ERROR: {message}");
+        }
+
+        public void WARNING(string message = "")
+        {
+            if (_levelLogging >= Level.WARNING)
+                File.AppendAllText(_pathToLogFile,
+                    $"{DateTime.Now.ToShortTimeString()} WARNING: {message}");
+        }
+
+        public void FATAL(string message = "")
+        {
+            if (_levelLogging >= Level.FATAL)
+                File.AppendAllText(_pathToLogFile,
+                    $"{DateTime.Now.ToShortTimeString()} FATAL: {message}");
+        }
+    }
+}
--- Telegram/Program.cs
+++ Telegram/Program.cs
@@ -37,8 +37,7 @@ namespace Telegram
             if (e.Message.Text != null)
             {
                 Console.WriteLine($"Получено письмо из чата {e.Message.Chat.Id}.");
-
-                Processing.Get(e.Message.Text, e.Message.MediaGroupId, out bool onLastLetter, out bool cityIsUsed, out string outCity, out int letterNumberFromEnd, out string wikiUrl,
+                Processing.Get(e.Message.Text, "Telegram." + e.Message.Chat.Id.ToString(), out bool onLastLetter, out bool cityIsUsed, out string outCity, out int letterNumberFromEnd, out string wikiUrl,
             out string yandexUrl, out string googleUrl, out string mapUrl, out (double latitude, double longitude) coordinateCity, out string photoUrl);
                 if (outCity == null)
                     await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: $"Город {e.Message.Text} не найден в базе данных, попробуйте ввести другой город на эту же букву");
