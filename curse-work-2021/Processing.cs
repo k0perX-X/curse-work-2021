ab3b65766463dd334e8faa052ffc155f0a8649c1
Николай Перминов <kolya-perminov@ya.ru>
2021-06-13 16:13:53 +0500

    Одна буква баг

--- curse-work-2021/Processing.cs
+++ curse-work-2021/Processing.cs
@@ -136,7 +136,7 @@ namespace Database
             var u2 = 1.0 - random.NextDouble();
             var rand = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
 
-            while (!(rand >= left && rand <= right))
+            while (!(left <= rand && rand < right))
             {
                 u1 = 1.0 - random.NextDouble();
                 u2 = 1.0 - random.NextDouble();
@@ -222,7 +222,7 @@ namespace Database
                             if (except.Count != 0)
                             {
                                 int numberOfCity =
-                                    (int)Math.Round((NormalRandom() - 1 / (except.Count * 2)) * except.Count);
+                                    (int)Math.Round((NormalRandom() - 1d / (except.Count * 2)) * except.Count);
                                 outCity = except[numberOfCity]; // используется смещенное нормальное распределение чтобы давать более редкие города чаще
 
                                 databaseUsers[id].UsedCities[c].Add(new City() { Name = city });
