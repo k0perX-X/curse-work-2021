8187c8dd1198030be241f4be0e7fdd3b17672288
Orlov Yasha <orlovaska599@gmail.com>
2021-06-13 12:03:56 +0300

    Начало описания тел бота

--- Telegram/Program.cs
+++ Telegram/Program.cs
@@ -10,12 +10,12 @@ namespace Telegram
         static ITelegramBotClient botClient;
         static void Main()
         {
-            Processing.Get(string city, string id, out bool cityIsUsed, out string outCity, out int letterNumberFromEnd, out string wikiUrl,
-            out string yandexUrl, out string googleUrl, out string mapUrl, out (double latitude, double longitude) coordinateCity, out string photoUrl);
+            
             var botClient = new TelegramBotClient(Configuration.BotToken);
             var me = botClient.GetMeAsync().Result;
             Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
 
+            botClient.OnMessage += Bot_FirstOnMessage;
             botClient.OnMessage += Bot_OnMessage;
             botClient.StartReceiving();
 
@@ -27,16 +27,25 @@ namespace Telegram
             Console.ReadKey();
         }
 
+        static void Bot_FirstOnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Введите первый город");
+        }
+
         static async void Bot_OnMessage(object sender, MessageEventArgs e)
         {
             if (e.Message.Text != null)
             {
                 Console.WriteLine($"Получено письмо из чата {e.Message.Chat.Id}.");
 
-                await botClient.SendTextMessageAsync(
-                  chatId: e.Message.Chat,
-                  text: "You said:\n" + e.Message.Text
-                );
+                Processing.Get(e.Message.Text, e.Message.MediaGroupId, out bool onLastLetter, out bool cityIsUsed, out string outCity, out int letterNumberFromEnd, out string wikiUrl,
+            out string yandexUrl, out string googleUrl, out string mapUrl, out (double latitude, double longitude) coordinateCity, out string photoUrl);
+                if (outCity == null)
+                    await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: $"Город {e.Message.Text} не найден в базе данных, попробуйте ввести другой город на эту же букву");
+                else if (cityIsUsed == true)
+                    await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: $"Вы уже использовали город {e.Message.Text}, попробуйте ввести другой город на эту же букву");
+                    else
+                await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "You said:\n" + e.Message.Text);
             }
         }
     }
